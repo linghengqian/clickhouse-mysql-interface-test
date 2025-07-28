e7204007d2ed75895db8a6e316e59270e627b633
linghengqian <linghengqian@outlook.com>
2025-07-28 12:14:59 +0800

    Remove HikariCP

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # clickhouse-mysql-interface-test
 
 - For https://github.com/apache/shardingsphere/issues/36081 .
+- Execute the following command on the Windows 11 instance with `PowerShell/PowerShell`,
+  `version-fox/vfox`, `git-for-windows/git` and `rancher-sandbox/rancher-desktop` installed.
+
+```shell
+vfox add java
+vfox install java@24.0.2-graalce
+vfox use --global java@24.0.2-graalce
+
+git clone git@github.com:linghengqian/clickhouse-mysql-interface-test.git
+cd ./clickhouse-mysql-interface-test/
+./mvnw -T 1C clean test
+```
+
+The log is as follows,
+
+```shell
+
+```
--- src/test/java/io/github/linghengqian/MySQLInterfaceTest.java
+++ src/test/java/io/github/linghengqian/MySQLInterfaceTest.java
@@ -1,42 +1,37 @@
 package io.github.linghengqian;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-import org.awaitility.Awaitility;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.MountableFile;
 
 import java.nio.file.Paths;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.concurrent.TimeUnit;
 
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SuppressWarnings({"resource", "SqlNoDataSourceInspection"})
 class MySQLInterfaceTest {
     @Test
     void assertShardingInLocalTransactions() throws SQLException {
-        try (GenericContainer<?> container = new GenericContainer<>("clickhouse/clickhouse-server:25.4.5.24")
-                .withEnv("CLICKHOUSE_USER", "changeme")
-                .withEnv("CLICKHOUSE_DEFAULT_ACCESS_MANAGEMENT", "1")
-                .withEnv("CLICKHOUSE_PASSWORD", "changeme")
-                .withEnv("CLICKHOUSE_DB", "my_database")
+        try (GenericContainer<?> container = new GenericContainer<>("clickhouse/clickhouse-server:25.6.5.41")
+                .withEnv("CLICKHOUSE_SKIP_USER_SETUP", "1")
                 .withCopyFileToContainer(
-                        MountableFile.forHostPath(Paths.get("src/test/resources/xml/mysql-interface.xml").toAbsolutePath()),
-                        "/etc/clickhouse-server/config.d/mysql-interface.xml")
-                .withExposedPorts(9004)) {
+                        MountableFile.forHostPath(Paths.get("src/test/resources/xml/interface.xml").toAbsolutePath()),
+                        "/etc/clickhouse-server/config.d/interface.xml")
+                .withExposedPorts(9004, 8123)) {
             container.start();
-            String jdbcUrl = "jdbc:mysql://localhost:" + container.getMappedPort(9004) + "/my_database";
-            HikariConfig hikariConfig = new HikariConfig();
-            hikariConfig.setJdbcUrl(jdbcUrl);
-            hikariConfig.setUsername("changeme");
-            hikariConfig.setPassword("changeme");
-            Awaitility.await().timeout(2L, TimeUnit.MINUTES).pollDelay(1L, TimeUnit.MINUTES).until(() -> true);
-            try (HikariDataSource hikariDataSource = new HikariDataSource(hikariConfig);
-                 Connection connection = hikariDataSource.getConnection()) {
+            String mysqlJdbcUrl = "jdbc:mysql://localhost:" + container.getMappedPort(9004) + "/default";
+            String clickhouseJdbcUrl = "jdbc:ch://localhost:" + container.getMappedPort(8123) + "/default";
+            await().atMost(1L,TimeUnit.MINUTES).ignoreExceptions().until(()->{
+                DriverManager.getConnection(clickhouseJdbcUrl,"default","").close();
+                return true;
+            });
+            try (Connection connection = DriverManager.getConnection(mysqlJdbcUrl,"default","")) {
                 ResultSet resultSet = connection.createStatement().executeQuery("SHOW VARIABLES LIKE 'version_comment'");
                 while (resultSet.next()) {
                     assertNotNull(resultSet.getString("Value"));
